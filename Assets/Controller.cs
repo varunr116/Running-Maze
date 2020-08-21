20bafaceb4deec75068c2d2bf2567365ae50cd9a
varunr116 <39424474+varunr116@users.noreply.github.com>
2020-08-21 19:26:35 +0530

    Moving agent

---
+++ Assets/Controller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+    public NavMeshAgent playerAgent;
+    [SerializeField]
+    GameObject lastPos;
+    bool isMoving;
+    void Update()
+    {
+        Debug.Log(isMoving);
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            isMoving = true;
+            
+
+        }
+         if (Input.GetMouseButtonUp(0))
+        {
+            isMoving = false;
+        }
+       
+        if (isMoving)
+        {
+            playerAgent.isStopped = false;
+             playerAgent.SetDestination(lastPos.transform.position);
+        }
+        else
+        {
+            playerAgent.isStopped = true; 
+        }
+    }
+  
+}
